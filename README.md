01c5af0a2a08829270e6d3750cd316af57ac4026
Vikas Sharma <93978146+vikasharma005@users.noreply.github.com>
2023-08-22 12:45:06 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,65 @@
+# Streamlit Fun Apps
+This repo contains few apps/games developed in Streamlit.
+
+___
+## Guessing Game
+
+> Project Location: `apps/guess/main.py`
+
+This project takes a maximum number from the user. Then it chooses a random number between 1 and the given maximum number.
+Now, it is the user's turn to guess the selected random number. By each try, app would hint if its number is higher or lower.
+One the number is guessed correctly, it would tell how many times it has been tried.
+
+## Tic-Tac-Toe (Offline)
+
+> Project Location: `apps/tictactoe/main.py`
+
+This is a Tic-Tac-Toe 3x3 offline against the system.
+
+## Tic-Tac-Toe (Online)
+
+> Project Location: `apps/tictactoe_online/main.py`
+
+This is a Tic-Tac-Toe 3x3 online against another online user. Each online user would select either `X` or `O`. 
+
+## Message Board
+
+> Project Location: `apps/chat/main.py`
+
+This is an online chat-box among online users.
+
+## Whiteboard (Online)
+
+> Project Location: `apps/whiteboard/main.py`
+
+This is an online whiteboard base on the user's session ID. We can also watch the other whiteboards live.
+
+___
+## Demo
+You can access the demo version deployed on Streamlit server at:
+
+[https://fun-games.streamlitapp.com/](https://fun-games.streamlitapp.com/)
+
+___
+## Run
+In order to run this tool, you must have Streamlit installed on your machine/environment:
+
+    streamlit run app.py
+
+## Run on Docker
+This application is available on [Docker Hub](https://hub.docker.com/r/kavehbc/streamlit-apps), and it can be run directly using:
+
+    docker run -p 80:8501 kavehbc/streamlit-apps
+
+Once you run it, you can open it in your browser on [http://127.0.0.1](http://127.0.0.1).
+
+## GitHub Repo
+This project is open-source, and it is available on Github at [https://github.com/kavehbc/streamlit-apps](https://github.com/kavehbc/streamlit-apps).
+
+## Developer(s)
+
+Kaveh Bakhtiyari - [Website](http://bakhtiyari.com) | [Medium](https://medium.com/@bakhtiyari)
+  | [LinkedIn](https://www.linkedin.com/in/bakhtiyari) | [GitHub](https://github.com/kavehbc)
+
+## Contribution
+Feel free to join the open-source community and contribute to this repository.
---
+++ apps/chat/main.py
@@ -0,0 +1,84 @@
+import streamlit as st
+import datetime
+import asyncio
+from lib.streamlit_session import get_session_id, get_all_sessions
+
+current_session_id = get_session_id()
+
+
+def count_online_users():
+    session_infos = get_all_sessions()
+    session_counter = 0
+    for session_info in session_infos:
+        session_counter += 1
+    return session_counter
+
+
+def rerun_all(current_session_id):
+    session_infos = get_all_sessions()
+    for session_info in session_infos:
+        session_info.session.flush_browser_queue()
+        session_info.session.request_rerun()
+
+st.rerun = rerun_all
+
+
+@st.cache_resource()
+def message():
+    ts = datetime.datetime.now().timestamp()
+    return {"1": {"timestamp": ts, "user": "System", "message": "Welcome"}}
+
+
+def print_messages(plh_messages):
+    messages = message()
+    lst = list(messages.items())
+    msg_body = ""
+    for i in reversed(lst):
+        msg_ts = i[1]["timestamp"]
+        msg_datetime = datetime.datetime.fromtimestamp(msg_ts)
+        msg_user = i[1]["user"]
+        msg_text = i[1]["message"]
+        msg_body += f"**{msg_user}** ({msg_datetime})\n\n"
+        msg_body += f"> {msg_text}\n\n"
+    plh_messages.markdown(msg_body)
+
+
+async def watch(plh_messages):
+    while True:
+        messages = message()
+        if len(messages) != st.session_state.msg_count:
+            st.session_state.msg_count = len(messages)
+            print_messages(plh_messages)
+
+        _ = await asyncio.sleep(1)
+
+
+def chat():
+    st.write(f"Online users: **{count_online_users()}**")
+    username = st.text_input("What's your name?")
+    with st.form(key='chat_form', clear_on_submit=True):
+        your_message = st.text_input("What's your message?")
+        btn_send = st.form_submit_button(label='Send')
+
+    st.subheader("Chat Board")
+    st.write("")
+    messages = message()
+    st.session_state.msg_count = len(messages)
+    lst = list(messages.items())
+
+    last_user = lst[-1][1]["user"]
+    last_message = lst[-1][1]["message"]
+    if (len(username) > 0 and len(your_message) > 0) and (last_user != username or last_message != your_message):
+        ts = datetime.datetime.now().timestamp()
+        messages[len(messages) + 1] = {"timestamp": ts,
+                                       "user": username,
+                                       "message": your_message}
+        # st.rerun(current_session_id)
+
+    plh_messages = st.empty()
+    print_messages(plh_messages)
+    asyncio.run(watch(plh_messages))
+
+
+if __name__ == '__main__':
+    chat()
